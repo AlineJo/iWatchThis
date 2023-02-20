02c8e2e7e577ab375cecc9f7447da152a78ef64c
AlineJo <josephali1993@gmail.com>
2023-02-20 07:02:01 +0400

    + constants file

---
+++ PHP/constents.php
@@ -0,0 +1,21 @@
+<?php
+
+//JSON KEYS
+$KEY_title = "title";
+$KEY_website = "website";
+$KEY_status = "status";
+$KEY_description = "description";
+$KEY_contentLink = "contentLink";
+$KEY_imgSrc = "imgSrc";
+$KEY_season = "season";
+$KEY_episode = "episode";
+$KEY_ganrasArray = "ganrasArray";
+
+
+// View KEYS
+$VIEW_ViewContentAll = "ViewContentAll";
+$VIEW_ViewContentPlaned = "ViewContentPlaned";
+$VIEW_ViewContentNow = "ViewContentNow";
+$VIEW_ViewContentDone = "ViewContentDone";
+
+?>
