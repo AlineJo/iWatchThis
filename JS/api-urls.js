da710c99f24a84bc7463c07146b68f5740becfb4
AlineJo <josephali1993@gmail.com>
2023-03-04 00:06:33 +0400

    + api-urls

---
+++ JS/api-urls.js
@@ -0,0 +1,20 @@
+export const HTTP_METHOD_GET_NO_CACHE = {
+    method: 'GET', headers: { 'Cache-Control': 'no-cache' }
+}
+
+export function HTTP_METHOD_POST_NO_CACHE(jsonRequestObj) {
+    const httpObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache'
+        },
+        // your expected POST request payload goes here
+        body: JSON.stringify(jsonRequestObj)
+    };
+    return httpObj;
+}
+
+
+// ******** userAPI URLS ***********//
+export const API_URL_GET_PLANNED_CONTENT = "http://localhost/expprojects/iWatchThis/php/get-content-planned.php"
