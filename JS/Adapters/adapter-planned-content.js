cb53bcfc61247db206eb12c25b6f80816497389b
AlineJo <josephali1993@gmail.com>
2023-03-04 00:07:54 +0400

    #fetch from PLANNED_API and display items

---
+++ JS/Adapters/adapter-planned-content.js
@@ -0,0 +1,56 @@
+import { fetchPlannedContent } from "../Fetches/fetch-planned-content.js";
+
+
+
+let plannedContainer = document.getElementById("plannedContainer")
+let cardTemplate = document.getElementById("cardTemplate")
+
+fetchPlannedContent();
+
+export function contentAdapter(content) {
+
+    content.forEach(contentItem => {
+        const clone = cardTemplate.content.cloneNode(true)
+        
+        let contentCard = clone.getElementById("contentCard")
+        let contentImg = clone.getElementById("contentImg")
+        let contentTitle = clone.getElementById("contentTitle")
+        let contentDescription = clone.getElementById("contentDescription")
+        let contentSeasone = clone.getElementById("contentSeason")
+        let contentEpisode = clone.getElementById("contentEpisode")
+        let contentWebsite = clone.getElementById("contentWebsite")
+
+        /*
+       [{
+        "id": 1,
+        "title": "post man test399",
+        "websiteName": "postman.com",
+        "status": "planned",
+        "description": "some deec",
+        "contentLink": "https://9animetv.to/watch/my-hero-academia-season-6-18154?ep=97604",
+        "imgSrc": "https://img.bunnycdnn.ru/_r/300x400/100/20/b7/20b7580c1abdf45a6eaf4826fc9fdf33/20b7580c1abdf45a6eaf4826fc9fdf33.jpg",
+        "genra": "Action, Fantasy, School",
+        "season": 6,
+        "episode": 20
+    }]
+        */
+
+        contentImg.src = contentItem.imgSrc
+        contentTitle.innerText = contentItem.title
+        contentDescription.innerText = contentItem.description
+        contentSeasone.innerText = "Season "+contentItem.season
+        contentEpisode.innerText = "Episode "+contentItem.episode
+        contentWebsite.innerText = contentItem.websiteName
+
+        contentCard.onclick = function(){
+            window.open(contentItem.contentLink, "_blank")
+        }
+
+        plannedContainer.append(clone)
+    });
+}
+
+
+
+
+
---
+++ JS/Fetches/fetch-planned-content.js
@@ -0,0 +1,21 @@
+import { contentAdapter } from "../Adapters/adapter-planned-content.js";
+import { API_URL_GET_PLANNED_CONTENT, HTTP_METHOD_GET_NO_CACHE } from "../api-urls.js";
+
+export async function fetchPlannedContent() {
+
+
+    // fetch response using API URL and HTTP method
+    const response = await fetch(API_URL_GET_PLANNED_CONTENT, HTTP_METHOD_GET_NO_CACHE);
+
+    // if error display error massege
+    if (!response.ok) {
+        throw new Error(`HTTP error! status : ${response.status}`)
+    }
+
+    // extract json from the response
+    const content = await response.json();
+
+    console.log(JSON.stringify(content));
+
+    contentAdapter(content)
+}
