290776d85e3ccb75d55e338af45dabea4a2ed39c
AlineJo <josephali1993@gmail.com>
2023-03-27 13:33:55 +0400

    # fix genraQuery issue

--- PHP/get-content-all.php
+++ PHP/get-content-all.php
@@ -6,8 +6,8 @@ $contnetQuery = "SELECT * FROM $VIEW_ViewContentAll";
 $contnetQueryResult= $cnn->query($contnetQuery)->fetchAll(PDO::FETCH_ASSOC);
 
 
-$$genraQuery = "SELECT * FROM `ContentGenra`";
-$genraQueryResult= $cnn->query($$genraQuery)->fetchAll(PDO::FETCH_ASSOC);
+$genraQuery = "SELECT * FROM `ContentGenra`";
+$genraQueryResult= $cnn->query($genraQuery)->fetchAll(PDO::FETCH_ASSOC);
 
 // create response object that contain both the food item and its extras
 $jsonResponse = new stdClass();
